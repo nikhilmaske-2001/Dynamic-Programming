35b894fca05b3ab1b33f2caff557b463f681b32f
Nikhil <nikhilmaske2001@gmail.com>
2020-10-25 12:55:45 +0530

    Added 3rd question

---
+++ 3)Equal sum partition/equalsumpartitiondp.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define pb push_back
+#define mod 1000000007
+#define INF 1000000000
+#define vi vector<ll>
+#define mll map<ll,ll>
+#define test() ll t;cin>>t;while(t--)
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define rep(a,n) ll a[n];for(i=0;i<n;i++)cin>>a[i];
+#define even(s) (s%2==0)
+#define odd(s) (s&1)
+#define sz(a) (ll)a.size()
+using namespace std;
+ll i,j;
+bool dp[1001][1001];
+
+bool equalsumpartition(int a[],int n,int sum)
+{
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=sum;j++)
+        {
+            if(a[i-1]<=j)
+            {
+                dp[i][j]=dp[i-1][j-a[i-1]] || dp[i-1][j];
+            }
+            else if(a[i-1]>j)
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][sum];
+}
+
+int main() {
+    fastio;
+    int n;
+    cin>>n;
+    int a[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        sum+=a[i];
+    }
+    if(odd(sum))
+    {
+        cout<<"NO"<<endl;
+    }
+    else
+    {
+        for(i=0;i<=n;i++)
+        {
+            dp[i][0]=true;
+        }
+        for(j=1;j<=sum;j++)
+        {
+            dp[0][j]=false;
+        }
+        bool flag=equalsumpartition(a,n,sum);
+        if(flag)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+        
+    }
+    
+    return 0;
+}
---
+++ 3)Equal sum partition/equalsumpartitionrecursion.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define pb push_back
+#define mod 1000000007
+#define INF 1000000000
+#define vi vector<ll>
+#define mll map<ll,ll>
+#define test() ll t;cin>>t;while(t--)
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define rep(a,n) ll a[n];for(i=0;i<n;i++)cin>>a[i];
+#define even(s) (s%2==0)
+#define odd(s) (s&1)
+#define sz(a) (ll)a.size()
+using namespace std;
+ll i,j;
+
+bool equalpartition(int a[],int n,int sum)
+{
+    if(sum==0)
+    {
+        return true;
+    }
+    if(n==0 && sum!=0)
+    {
+        return false;
+    }
+    if(a[n-1]<=sum)
+    {
+        return equalpartition(a,n-1,sum-a[n-1]) || equalpartition(a,n-1,sum);
+    }
+    else if(a[n-1]>sum)
+    {
+        return equalpartition(a,n-1,sum);
+    }
+}
+
+int main() {
+    fastio;
+    int n;
+    cin>>n;
+    int a[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        sum+=a[i];
+    }
+    if(odd(sum))
+    {
+        cout<<"NO"<<endl;
+    }
+    else
+    {
+        bool flag=equalpartition(a,n,sum);
+        if(flag)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+        
+    }
+    
+    return 0;
+}
